058f568f966345763c448c63bc0d971aacf191dd
Alexey Nikolaev <79050924+Inm4rt@users.noreply.github.com>
2023-01-14 12:26:44 +0500

    переделал работу с объектами

--- src/main/java/com/example/task/Controller/WebController.java
+++ src/main/java/com/example/task/Controller/WebController.java
@@ -1,42 +1,35 @@
 package com.example.task.Controller;
 
-import com.example.task.model.Person;
+import com.example.task.dto.PersonDto;
 import com.example.task.service.PesonService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Optional;
-
 @RestController
 public class WebController {
     @Autowired
     private PesonService personService = new PesonService();
-    @Autowired
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
-    private ObjectMapper objectMapper;
 
     @ResponseStatus(HttpStatus.OK)
     @GetMapping("/")
-    public Iterable<Person> showAllPerson() {
+    public Iterable<PersonDto> showAllPerson() {
         return personService.getAllPerson();
     }
 
     @GetMapping("/one")
-    public ResponseEntity showOnePerson(@RequestParam(name = "id") long id, Person person){
-        Optional<Person> searchPerson = personService.getOnePerson(id);
-        if (searchPerson.isEmpty()) {
+    public ResponseEntity showOnePerson(@RequestParam(name = "id") long id) {
+        PersonDto searchPerson = personService.getOnePerson(id);
+        if (searchPerson.getFirstName() == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(searchPerson);
         }
         return ResponseEntity.status(HttpStatus.OK).body(searchPerson);
     }
 
     @PutMapping("/update")
-    public ResponseEntity updatePerson(@Valid @RequestBody Person person) {
+    public ResponseEntity updatePerson(@Valid @RequestBody PersonDto person) {
         personService.updatePerson(person);
         return ResponseEntity.status(HttpStatus.OK).body(person);
     }
@@ -47,7 +40,7 @@ public class WebController {
     }
 
     @PostMapping("/")
-    public ResponseEntity AddPerson(@Valid @RequestBody Person person) {
+    public ResponseEntity AddPerson(@Valid @RequestBody PersonDto person) {
 
         personService.addPerson(person);
         return ResponseEntity.status(HttpStatus.CREATED).body("Valid");
--- src/main/java/com/example/task/service/PesonService.java
+++ src/main/java/com/example/task/service/PesonService.java
@@ -1,34 +1,39 @@
 package com.example.task.service;
 
+import com.example.task.dto.PersonDto;
 import com.example.task.model.Person;
 import com.example.task.repository.PersonRepository;
+import com.example.task.utils.MappingUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class PesonService {
     @Autowired
     private PersonRepository personRepository;
 
-    public void addPerson(Person person) {
-        personRepository.save(person);
+    private final MappingUtils mappingUtils = new MappingUtils();
+
+    public void addPerson(PersonDto person) {
+        personRepository.save(mappingUtils.mapToPerson(person));
     }
 
-    public Optional<Person> getOnePerson(long id) {
-        return personRepository.findById(id);
+    public PersonDto getOnePerson(long id) {
+        return mappingUtils.mapToPersonDTO(personRepository.findById(id).orElse(new Person()));
     }
 
-    public Iterable<Person> getAllPerson() {
-        return personRepository.findAll();
+    public Iterable<PersonDto> getAllPerson() {
+        Iterable<PersonDto> a = personRepository.findAll().stream().map(mappingUtils::mapToPersonDTO).collect(Collectors.toList());
+        return a;
     }
 
     public void deletePerson(long id) {
         personRepository.deleteById(id);
     }
 
-    public void updatePerson(Person person) {
-        personRepository.save(person);
+    public void updatePerson(PersonDto person) {
+        personRepository.save(mappingUtils.mapToPerson(person));
     }
 }
